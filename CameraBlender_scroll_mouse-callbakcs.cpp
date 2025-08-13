95bbf072169b52b7e625ff15b72bba5e1d0da8a1
int 21jh <145056912+Gchipunov@users.noreply.github.com>
2025-08-12 20:33:25 -0700

    Create CameraBlender_scroll_mouse-callbakcs.cpp

---
+++ CameraBlender_scroll_mouse-callbakcs.cpp
@@ -0,0 +1,22 @@
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(yoffset);
+}
